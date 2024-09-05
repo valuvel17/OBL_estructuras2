cc395282e2cc88028a1ea0032505b6207d4787c8
valuvel17 <valentina.velazquez1706@gmail.com>
2024-09-05 11:23:45 -0300

    ejercicio 1 primer version

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,10 +3,39 @@
 #include <iostream>
 #include <limits>
 #include "clases/Libreria.cpp"
+#include "funciones/chars.cpp"
 
 using namespace std;
 
 int main(){
-    
+    Libreria* nueva = new Libreria;
+    int cantidad_operaciones = 0;
+    cin >> cantidad_operaciones;
+    for(int i=0; i<cantidad_operaciones; i++){
+        char* aux = "";
+        cin >> aux;
+        char** auxChar= separarChars(aux);
+        char letra = aux[0];
+        switch (letra){
+            case 'A':
+                nueva->add((int)auxChar[1],auxChar[2]);
+            break;
+            
+            case 'F':
+                cout << nueva->find((int)auxChar[1]) << endl;
+            break;
+
+            case 'T':
+                if (!nueva->toggle((int)auxChar[1])) cout << "libro_no_encontrado" << endl;
+            break;
+
+            case 'C':
+                cout << nueva->cantidadTotal() << " " << nueva->cantidadHabilitados() << " " << nueva->cantidadDeshabilitados() << endl;
+            break;
+            default:
+                cout << "comando incompatible" << endl;
+            break;
+        }
+    }
     return 0;
 }
---
+++ funciones/chars.cpp
@@ -0,0 +1,31 @@
+char** separarChars(char* cadena) {
+    int cantidad_palabras = 1;
+    int largo = 0;
+
+    while(cadena[largo] != '\0'){
+        if(cadena[largo] == ' ') cantidad_palabras++;
+        largo++;
+    }
+    largo++;
+
+    char** resultado = new char*[cantidad_palabras];
+    int inicioPalabra = 0;
+    int indicePalabra = 0;
+
+    for (int i = 0; i<largo ; i++) {
+        if (cadena[i] == ' '){
+            int finPalabra = i;
+            int longitudPalabra = finPalabra - inicioPalabra;
+            resultado[indicePalabra] = new char[longitudPalabra + 1];
+            
+            for (int j = 0; j < longitudPalabra; j++) {
+                resultado[indicePalabra][j] = cadena[inicioPalabra + j];
+            }
+
+            resultado[indicePalabra][longitudPalabra] = '\0';            
+            indicePalabra++;
+            inicioPalabra = i + 1;
+        }
+    }
+    return resultado;
+}
