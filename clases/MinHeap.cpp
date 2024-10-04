b137800cb4691ba71f93145ab427c6485a947a9e
SalvaDepons18 <salva.depons.18@gmail.com>
2024-10-04 19:28:58 -0300

    funciones comentadas

--- clases/MinHeap.cpp
+++ clases/MinHeap.cpp
@@ -18,18 +18,26 @@ class MinHeap{
     int espacio; //no necesariamente tamano de array (ignoramos el 0 en nuestra implementacion)
     //funciones privadas
    
+    // PRE: 
+    // POS: Retorna si esta Lleno el heap
     bool estaLleno(){
         return tope >= espacio;
     }
 
+    // PRE: 
+    // POS: Retorna si esta vacio el heap
     bool estaVacio(){
         return tope == 1;
     }
-   
+    
+    // PRE: Recibe una posicion en el heap
+    // POS: Devuelve el padre del objeto en esa posicion
     datoHeap padre(int posHijo){
         return arrayHeap[posHijo/2];
     }
 
+    // PRE: Recibe 2 objetos
+    // POS: Devuelve true si no se cumple la condicion dek heap
     bool comparar(datoHeap padre, datoHeap hijo){
         bool ret = false; //true si es el hijo mas chico y false si es el padre mas chico
         if (hijo.precio < padre.precio) ret = true;
@@ -39,6 +47,8 @@ class MinHeap{
         return ret;
     }
 
+    // PRE: Recibe 2 posiciones
+    // POS: Intercambia los objetos de esas posiciones
     void swapi(int pos1, int pos2){
         datoHeap aux = arrayHeap[pos1];
         arrayHeap[pos1]=arrayHeap[pos2];
@@ -47,6 +57,8 @@ class MinHeap{
         arrayId[arrayHeap[pos2].id] = pos2;
     }
 
+    // PRE: Recibe una posicion en el heap
+    // POS: Flota el objeto de esa posicion hasta que respete la condicion del heap
     int flotar(int pos) {
         while (pos > 1 && comparar(arrayHeap[pos / 2], arrayHeap[pos])) {
             swapi(pos, pos / 2);
@@ -55,6 +67,8 @@ class MinHeap{
         return pos; 
     }
 
+    // PRE: Recibe una posicion en el heap
+    // POS: Hunde el objeto de esa posicion hasta que respete la condicion del heap
     void hundir(int pos) {
         int posIzq = 2 * pos;
         int posDer = 2 * pos + 1;
@@ -67,7 +81,8 @@ class MinHeap{
         }
     }
 
-
+    // PRE: Recibe un id y un precio
+    // POS: Agrega un nuevo objeto al heap con ese id y el precio
     void addAux(int id,int precio){
         if(!estaLleno()){
             if(arrayId[id] == -1){
@@ -84,6 +99,8 @@ class MinHeap{
         }
     }
     
+    // PRE:
+    // POS: Elimina el objeto con menor precio  
     void removeAux(){
         swapi(1,tope);  
         tope--;
@@ -91,7 +108,7 @@ class MinHeap{
     }
     
     public:
-    //constructores
+    // Constructor
     MinHeap(int size){
         arrayHeap = new datoHeap[size+1];
         for (int i = 0; i < size+1; i++){
@@ -105,13 +122,16 @@ class MinHeap{
             this->arrayId[i] = -1;
         }
     }
-
+    
+    // Destructor 
     ~MinHeap(){
         delete[] this->arrayHeap;
         delete[] this->arrayId;
     }
     //funciones publicas
 
+    // PRE: 
+    // POS: Funcion que ayuda a debuggear 
     void mostrarHeap(){
         cout << "elHeap se ve: ";
         cout << "[ ";
@@ -122,17 +142,22 @@ class MinHeap{
         
     } 
 
-
+    // PRE: Recibe un id y un precio
+    // POS: Agrega un nuevo objeto al heap con ese id y el precio
     void add(int id, int precio){
         addAux(id,precio);
         //mostrarHeap();
     }
     
+    // PRE:
+    // POS: Elimina el objeto con menor precio  
     void remove(){
         removeAux();
        // mostrarHeap();
     }
 
+    // PRE:
+    // POS: Devuelve el objeto con menor precio  
     int peak(){
         return this->arrayHeap[1].id;
     }
